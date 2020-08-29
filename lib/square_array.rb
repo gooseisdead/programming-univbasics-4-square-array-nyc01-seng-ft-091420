48de578d57e5e621e02facb624febbf7a41536c9
Mike Smith <gooseisdead@mac.com>
2020-08-29 19:22:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   new_array = []
   counter = 0
   while counter < array.length
-    array.index do |i| 
+    array.index do |i|
       new_array << { i ** 2 }
     end
   end
